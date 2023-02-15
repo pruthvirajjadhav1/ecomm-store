74fd7783aefe4f2b490326d214ce38232105a1cd
pruthvirajjadhav1 <pruthvirajjadhav1@gmail.com>
2023-02-15 14:44:12 +0530

    feat: cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,19 @@
 const BigPromise = require('../middlewares/bigPromise')
+const User = require("../models/user")
+const cookieToken = require("../utils/cookieToken")
 
-exports.signup = BigPromise((req,res)=>{
-	res.status(200).json("WORKING FINE");
+exports.signup = BigPromise(async (req,res,next)=>{
+	const {name,email,password} = req.body;
+
+	if(!email || !name || !password){
+		return next(new Error("Someting is missing"));
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password
+	})
+
+	cookieToken(user,res);
 })
