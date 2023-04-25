385271bc9fdde44c14db383c10d56adaa4912f0a
Pruthviraj jadhav <73253354+pruthvirajjadhav1@users.noreply.github.com>
2023-04-25 11:47:10 +0000

    Update index.js and user.js

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,19 +1,16 @@
-const bigPromise = require('./bigPromise');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
+const bigPromise = require("./bigPromise");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 exports.isLoggedIn = bigPromise(async (req, res, next) => {
-  const token =
-    req.cookies.token || req.header('Authorization').replace('Bearer ', '');
+  const token = req.cookies.token;
 
   if (!token) {
-    return next(new Error('Login first to reach the access page', 401));
+    return next(new Error("Login first to reach the access page", 401));
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
   req.user = await User.findById(decoded.id);
-
   next();
 });
 
